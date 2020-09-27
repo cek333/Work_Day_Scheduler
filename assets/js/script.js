2ea677691ce82f7438aeaac3755039873fc60cf4
cek333 <69444825+cek333@users.noreply.github.com>
2020-09-27 18:16:16 -0400

    added colour coding of textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,40 @@
 let schedule; // array of scheduler entries. Array entries 0 - 8 are ignored.
+let m = moment(); // moment object
 
 function saveEntry(event) {
-  console.log(`[saveEntry] event triggered`);
+  // console.log(`[saveEntry] event triggered`);
   // get handle to <div class="row">
   let divRow = $(event.target).parents('.row');
   let id = divRow.attr('id').split('-')[1];
-  console.log(`[saveEntry] id:${id}`, divRow)
+  // console.log(`[saveEntry] id:${id}`, divRow)
   // save entry
   let entry = divRow.find('textarea').val();
   if (entry != "") {
     schedule[id] = entry;
     // update localStorage
     localStorage.setItem('schedule', JSON.stringify(schedule));
-    console.log(`[saveEntry] entry stored`);
+    // console.log(`[saveEntry] entry stored`);
   }
 }
 
-// function colourCodeEntries() {
-//   // get current hour
-// }
+function colourCodeEntries() {
+  // get current hour
+  let curHr = m.format('H');
+  for (let idx=9; idx < 18; idx++) {
+    if (idx==curHr) {
+      $(`#hour-${idx} textarea`).addClass('present');
+    } else if (idx < curHr) {
+      $(`#hour-${idx} textarea`).addClass('past');
+    } else {
+      // idx > curHr
+      $(`#hour-${idx} textarea`).addClass('future');
+    }
+  }
+}
 
 // Initialization
 // set the date
-$('#currentDay').text(moment().format('dddd, MMMM Do'));
+$('#currentDay').text(m.format('dddd, MMMM Do'));
 
 // initialize scheduler data
 let tmp = localStorage.getItem('schedule');
@@ -32,7 +44,7 @@ if (tmp == null) {
   schedule = JSON.parse(tmp);
 }
 
-//colourCodeEntries();
+colourCodeEntries();
 
 // display stored entries if any
 for (let idx=9; idx < schedule.length; idx++) {
